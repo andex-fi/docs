34573650f99edbb9a92141cbf27aa4f2ecc6147b
Nate <89227631+natedotv2@users.noreply.github.com>
2022-12-07 22:53:18 +0100

    Switched back to Uniswap Setup

---
+++ docs/sdk/v3/overview.md
@@ -0,0 +1,27 @@
+---
+id: overview
+sidebar_position: 1
+title: Overview
+---
+# The Uniswap V3 SDK
+
+> **Welcome to the V3 Uniswap SDK!**
+
+The Uniswap V3 SDK provides abstractions to assist you with interacting with the Uniswap V3 smart contracts in a Typescript/Javascript environment. It makes uses of the [**Core SDK**](../core//overview.md) to gain access to abstractions that are common amongst the Uniswap SDKs.
+
+These docs will help you better understand how to use the SDK and integrate it into your application.
+
+To begin, we recommend looking at the [**Guides**](./guides/01-quick-start) and for deeper reference see the [**Technical Reference**](./reference/overview).
+
+
+# Resources
+
+- [**V3 SDK Github Repo**](https://github.com/Uniswap/v3-sdk)
+- [**Core SDK Github Repo**](https://github.com/Uniswap/sdk-core)
+- [**V3 SDK NPM Package**](https://www.npmjs.com/package/@uniswap/v3-sdk)
+
+[![Unit Tests](https://github.com/Uniswap/uniswap-v3-sdk/workflows/Unit%20Tests/badge.svg)](https://github.com/Uniswap/uniswap-v3-sdk/actions?query=workflow%3A%22Unit+Tests%22)
+[![Lint](https://github.com/Uniswap/uniswap-v3-sdk/workflows/Lint/badge.svg)](https://github.com/Uniswap/uniswap-v3-sdk/actions?query=workflow%3ALint)
+[![npm version](https://img.shields.io/npm/v/@uniswap/v3-sdk/latest.svg)](https://www.npmjs.com/package/@uniswap/v3-sdk/v/latest)
+[![npm bundle size (scoped version)](https://img.shields.io/bundlephobia/minzip/@uniswap/v3-sdk/latest.svg)](https://bundlephobia.com/result?p=@uniswap/v3-sdk@latest)
+[![Discord](https://img.shields.io/badge/discord-join%20chat-blue.svg)](https://discord.com/channels/597638925346930701/607978109089611786)
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,132 +1,309 @@
-// @ts-check
-// Note: type annotations allow type checking and IDEs autocompletion
+const math = require('remark-math')
+const katex = require('rehype-katex')
+require('dotenv').config()
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
-
-/** @type {import('@docusaurus/types').Config} */
-const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
-
-  // Even if you don't use internalization, you can use this field to set useful
-  // metadata like html lang. For example, if your site is Chinese, you may want
-  // to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+module.exports = {
+  customFields: {
+    // Analytics proxy URL
+    analyticsProxyUrl: process.env.REACT_APP_AMPLITUDE_PROXY_URL,
+    // Determines if staging env
+    stagingEnv: process.env.REACT_APP_STAGING,
+    // From node
+    nodeEnv: process.env.NODE_ENV,
   },
+  title: 'Uniswap',
+  tagline: 'Documentation and Guides',
+  url: 'https://docs.uniswap.org/',
+  baseUrl: '/',
+  onBrokenLinks: 'warn',
+  onBrokenMarkdownLinks: 'ignore',
+  favicon: 'img/favicon.png',
+  organizationName: 'Uniswap', // Usually your GitHub org/user name.
+  projectName: 'Uniswap-docs', // Usually your repo name.
+  themeConfig: {
+    image: 'img/twitter_card_bg.jpg',
+    prism: {
+      additionalLanguages: ['solidity'],
+    },
+    algolia: {
+      apiKey: '32465e2ab6f7554ff014e64c0d92171c',
+      indexName: 'v3-docs',
+      appId: 'S0IDD0YGLZ',
+    },
+    navbar: {
+      title: 'Uniswap Docs',
+      logo: {
+        alt: 'Uniswap Unicorn',
+        src: 'img/uni_dark_icon.svg',
+      },
+      items: [
+        {
+          to: '/concepts/overview',
+          label: 'Concepts',
+          position: 'left',
+          className: 'V3_active',
+        },
+        {
+          to: '/contracts/v3/overview',
+          label: 'Contracts',
+          position: 'left',
+          className: 'V3_active',
+        },
+        {
+          to: '/sdk/v3/overview',
+          label: 'SDKs',
+          position: 'left',
+          className: 'V3_active',
+        },
+        {
+          to: '/api/subgraph/overview',
+          label: 'APIs',
+          position: 'left',
+          className: 'V3_active',
+        },
+        {
+          label: 'Give Feedback',
+          to: 'https://forms.gle/13XtjmkwdXQ2jMn26',
+          position: 'right',
+          className: 'persistent',
+        },
+        {
+          label: 'Whitepaper',
+          to: 'https://uniswap.org/whitepaper-v3.pdf',
+          position: 'right',
+          className: 'persistent',
+        },
+        {
+          href: 'https://github.com/uniswap/uniswap-docs',
+          label: 'GitHub',
+          position: 'right',
+          className: 'persistent',
+        },
+        {
+          href: 'https://unigrants.org/',
+          label: 'Grants',
+          position: 'right',
+          className: 'persistent',
+        },
+      ],
+    },
+    footer: {
+      // style: "dark",
+      links: [
+        {
+          title: 'Developers',
+          items: [
+            {
+              label: 'Feedback',
+              href: 'https://forms.gle/13XtjmkwdXQ2jMn26',
+            },
+            {
+              label: 'Bug Bounty',
+              href: 'https://github.com/Uniswap/uniswap-v3-periphery/blob/main/bug-bounty.md',
+            },
+            {
+              label: '#dev-chat',
+              href: 'https://discord.gg/ybKVQUWb4s',
+            },
+            {
+              label: 'Whitepaper',
+              href: 'https://uniswap.org/whitepaper-v3.pdf',
+            },
+          ],
+        },
+        {
+          title: 'Github',
+          items: [
+            {
+              label: 'uniswap-v3-core',
+              href: 'https://github.com/Uniswap/uniswap-v3-core',
+            },
+            {
+              label: 'uniswap-v3-sdk',
+              href: 'https://github.com/Uniswap/uniswap-v3-sdk',
+            },
+            {
+              label: 'uniswap-v3-periphery',
+              href: 'https://github.com/Uniswap/uniswap-v3-periphery',
+            },
+            {
+              label: 'Deployment addresses',
+              href: 'https://github.com/Uniswap/uniswap-v3-periphery/blob/main/deploys.md',
+            },
+          ],
+        },
+        {
+          title: 'Ecosystem',
+          items: [
+            {
+              label: 'Home',
+              href: 'https://uniswap.org/',
+            },
+            {
+              label: 'App',
+              href: 'https://app.uniswap.org/',
+            },
+            {
+              label: 'Analytics',
+              href: 'https://info.uniswap.org/home',
+            },
+            {
+              label: 'Token Lists',
+              href: 'https://tokenlists.org/',
+            },
+            {
+              label: 'Brand Assets',
+              href: 'https://uniswap.org/Uniswap_brand_assets.zip',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Governance',
+              href: 'https://gov.uniswap.org/',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discord.gg/ybKVQUWb4s',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/Uniswap',
+            },
+            {
+              label: 'Blog',
+              href: 'https://uniswap.org/blog/',
+            },
+          ],
+        },
+      ],
+      // copyright: `unlicensed`,
+    },
+    colorMode: {
+      // "light" | "dark"
+      defaultMode: 'dark',
 
+      // Hides the switch in the navbar
+      // Useful if you want to support a single color mode
+      disableSwitch: false,
+
+      // Should we use the prefers-color-scheme media-query,
+      // using user system preferences, instead of the hardcoded defaultMode
+      respectPrefersColorScheme: true,
+    },
+  },
   presets: [
     [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
+      '@docusaurus/preset-classic',
+      {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          id: 'contracts',
+          path: 'docs/contracts',
+          routeBasePath: 'contracts/',
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
+          editUrl: 'https://github.com/uniswap/uniswap-docs/tree/main/',
+          includeCurrentVersion: true,
         },
         blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          remarkPlugins: [math],
+          rehypePlugins: [katex],
+          path: 'blog/',
+          blogTitle: 'Engineering Blog',
+          blogSidebarCount: 0,
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
+          customCss2: require.resolve('./src/css/colors.css'),
         },
-      }),
+      },
     ],
   ],
+  stylesheets: [
+    {
+      href: 'https://cdn.jsdelivr.net/npm/katex@0.13.24/dist/katex.min.css',
+      type: 'text/css',
+      integrity: 'sha384-odtC+0UGzzFL/6PNoE8rX/SPcQDXBJ+uRepguP4QkPCm2LBxH3FA3y+fKSiJ+AmM',
+      crossorigin: 'anonymous',
+    },
+  ],
+  plugins: [
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'sdk',
+        path: 'docs/sdk',
+        routeBasePath: 'sdk',
+        remarkPlugins: [[require('@docusaurus/remark-plugin-npm2yarn'), { sync: true }]],
+        includeCurrentVersion: true,
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'api',
+        path: 'docs/api',
+        routeBasePath: 'api/',
+        remarkPlugins: [[require('@docusaurus/remark-plugin-npm2yarn'), { sync: true }]],
+        includeCurrentVersion: true,
+      },
+    ],
+    [
+      '@docusaurus/plugin-content-docs',
+      {
+        id: 'concepts',
+        path: 'docs/concepts',
+        routeBasePath: 'concepts/',
+        remarkPlugins: [[require('@docusaurus/remark-plugin-npm2yarn'), { sync: true }]],
+        includeCurrentVersion: true,
+      },
+    ],
+    [
+      '@docusaurus/plugin-client-redirects',
+      {
+        createRedirects(existingPath) {
+          // V3 Redirects
+          if (existingPath.includes('/concepts/overview')) {
+            return [existingPath.replace('/concepts/overview', '/protocol/introduction')]
+          }
+          if (existingPath.includes('/contracts/v3/reference')) {
+            return [existingPath.replace('/contracts/v3/reference', '/protocol/reference')]
+          }
+          if (existingPath.includes('/contracts/v3/guides')) {
+            return [existingPath.replace('/contracts/v3/guides', '/protocol/guides')]
+          }
+          // V2 Redirects
+          if (existingPath.includes('/contracts/v2/reference')) {
+            return [existingPath.replace('/contracts/v2/reference', '/protocol/V2/reference')]
+          }
+          if (existingPath.includes('/contracts/v2/guides')) {
+            return [existingPath.replace('/contracts/v2/guides', '/protocol/V2/guides')]
+          }
+          // Permit2 Redirects
+          if (existingPath.includes('/contracts/permit2')) {
+            return [existingPath.replace('/contracts/permit2', '/protocol/permit2')]
+          }
+          // v3-sdk Redirects
+          if (existingPath.includes('/sdk/v3/overview')) {
+            return [existingPath.replace('/sdk/v3/overview', '/sdk/introduction')]
+          }
+          if (existingPath.includes('/sdk/v3/guides')) {
+            return [existingPath.replace('/sdk/v3/guides', '/sdk/guides')]
+          }
+          // swap-widgets Redirects
+          if (existingPath.includes('/sdk/swap-widget/overview')) {
+            return [existingPath.replace('/sdk/swap-widget/overview', '/sdk/widgets/swap-widget')]
+          }
+          if (existingPath.includes('/sdk/swap-widget/reference/v2')) {
+            return [existingPath.replace('/sdk/swap-widget/reference/v2', '/sdk/widgets/swap-widget/api')]
+          }
 
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          // Return a falsy value: no redirect created
+          return undefined
         },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
       },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
-};
-
-module.exports = config;
+    ],
+  ],
+}
